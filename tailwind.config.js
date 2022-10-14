00b60da6acf7870cdf6a0cda94da2d174dda679d
Ed <ed.jones@gmail.com>
2022-10-14 08:53:40 -0400

    Configure tailwin.config.js per readme. But probably not correct.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{css,xml,html,vue,svelte,ts,tsx}'
+  ],
+  // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
+  darkMode: ['class', '.ns-dark'],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false // disables browser-specific resets
+  }
 }
