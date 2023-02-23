a4bf82a1ad802721491f47a8fd59db11e28f12d3
Ed <ed.jones@gmail.com>
2023-02-23 12:36:31 -0500

    Finish letterbox and add letterbox2

--- app/components/LetterBox2.vue
+++ app/components/LetterBox2.vue
@@ -16,14 +16,14 @@
             <Label class="text-2xl text-center" :text="pageInfo.challenge"  />
             <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text1" textWrap="true" />
             <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text2" textWrap="true" />
+            <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text3" textWrap="true" />
             <XPCard2 :xpObj="pageXPDetails[0]" ></XPCard2>
-            <Label class="text-base leading-none font-light  p-4 pb-0" text="Here, Dr. Strom points at a green area we call the 'Letter Box'. What's the more technical name for the brain's Letter Box?" textWrap="true" />
-            <TextViewWithHint width="350" height="70" class="text-black input-gray text-xl" editable="true" v-model="textViewValue1" hint="" returnKeyType="next" > </TextViewWithHint>
-            <Label class="text-base leading-none font-light  p-4 pb-0" text="The video shows a picture of the brain of a 9 year old non-reader ('dyslexic')." textWrap="true" />
-            <Label class="text-base leading-none font-light  p-4 pb-0" text="What's the difference between the two brains?" textWrap="true" />
+            <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text4" textWrap="true" />
+            <TextViewWithHint width="350" height="70" class="text-black input-gray text-xl mt-6" editable="true" v-model="textViewValue1" hint="" returnKeyType="next" > </TextViewWithHint>
+            <Label class="text-base leading-none font-light  p-4 pb-0" text="text here" textWrap="true" />
+            <Label class="text-base leading-none font-light  p-4 pb-0" text="text here" textWrap="true" />
             <TextViewWithHint width="350" height="70" class="text-black input-gray text-xl" editable="true" v-model="textViewValue2" hint="" returnKeyType="next" > </TextViewWithHint>
             <Button class="btn-b" text="Enter" @tap="acceptInput" /> 
-            <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text4" textWrap="true" />
             <Label class="text-base leading-none font-light p-4 pb-0" :text="pageInfo.text5" textWrap="true" />
              <!--XPCard2 :xpObj="pageXPDetails[1]" ></XPCard2--> 
           </StackLayout>
@@ -41,7 +41,7 @@
   import XPCard2 from "./XPCard2";
   import XPModalA from "./XPModalA";
   import { XPs } from "../data/xp_list.js";
-  import LetterBox2 from './LetterBox2';
+  import PathToReading from './PathToReading';
   //import P rogressBar from "./ProgressBar";
 import { topicPages } from "../data/pages_list.js";
 //import PreviousNextView from '@nativescript/iqkeyboardmanager';
@@ -54,16 +54,16 @@ const alertOptions = {
   }; 
   export default {
     mounted() {
-      SelectedPageService.getInstance().updateSelectedPage("LetterBox");
+      SelectedPageService.getInstance().updateSelectedPage("LetterBox2");
     },
     components: {
       XPCard2
   },
     data() {
-      const page="LetterBox";
+      const page="LetterBox2";
       const pageInfo = preparePageInfo(page, topicPages);
       //console.info("RewiringEngage>data(), pageInfo is: ", pageInfo);
-      console.info("In LetterBox>data, pageInfo[0].page is: ", pageInfo[0].page );
+      console.info("In LetterBox2>data, pageInfo[0].page is: ", pageInfo[0].page );
       const pageXPDetails = preparePageDetails(pageInfo[0], XPs);
 
     return {
@@ -85,7 +85,7 @@ const alertOptions = {
       }, 
       
       acceptInput() {
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$  LetterBox Input", this.textViewValue1,this.textViewValue2);
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$  Letterbox2 Input", this.textViewValue1,this.textViewValue2);
         let now = new Date();
         let docNum = now.getTime();
         console.log("Now: ",  docNum);
@@ -95,7 +95,7 @@ const alertOptions = {
 this.$store.commit('addXP', {XP: "XP3000"});
 */
 Dialogs.alert(alertOptions).then(() => {
-   this.$navigateTo(LetterBox2);
+   this.$navigateTo(PathToReading);
 })
 },
 
