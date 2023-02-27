994db6841630091001270fea524ab070545c5c78
Ed <ed.jones@gmail.com>
2023-02-27 12:13:58 -0500

    Start to object/data/component approach to tasks

---
+++ app/data/task_list.js
@@ -0,0 +1,56 @@
+export const tasks = [ 
+  {
+    taskID: 0,
+    title: "(Black) Literacy Matters--Intro",
+    challenge: "",
+    text1: "",
+    text2: "",
+    text3: "",
+    closingText: "",
+    closingText2: "", 
+    modal: ""
+  },
+  {
+    taskID: 1,
+    title: "(Black) Literacy Matters--Intro",
+    challenge: "",
+    text1: "",
+    text2: "",
+    text3: "",
+    closingText: "",
+    closingText2: "", 
+    modal: ""
+  },
+  {
+    taskID: 2,
+    title: "(Black) Literacy Matters--Intro",
+    challenge: "",
+    text1: "",
+    text2: "",
+    text3: "",
+    closingText: "",
+    closingText2: "", 
+    modal: ""
+  },
+  {
+    taskID: 3,
+    title: "(Black) Literacy Matters--Intro",
+    challenge: "",
+    text1: "",
+    text2: "",
+    text3: "",
+    closingText: "",
+    closingText2: "", 
+    modal: ""
+  },
+  {
+    taskID: 4,
+    title: "(Black) Literacy Matters--Intro",
+    challenge: "",
+    text1: "",
+    text2: "",
+    text3: "",
+    closingText: "",
+    closingText2: "", 
+    modal: ""
+  } ]
