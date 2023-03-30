04a4abde0f38f4ed70dabeef3c09b83f30c3d1c9
Ed <ed.jones@gmail.com>
2023-03-30 13:27:56 -0400

    Android settings

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'com.edwincjones.blackteenliteracymatters',
+  id: 'com.edwincjones.teenliteracymatters',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
